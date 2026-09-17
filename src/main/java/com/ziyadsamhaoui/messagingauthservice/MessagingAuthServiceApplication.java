068ac2dfb69f375dc15d108daad1992e91a55c22
Ziyad SAMHAOUI <ziyad.samhaoui@usms.ac.ma>
2026-09-17 13:07:17 +0100

    initial commit

---
+++ src/main/java/com/ziyadsamhaoui/messagingauthservice/MessagingAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ziyadsamhaoui/messagingauthservice/MessagingAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyadsamhaoui.messagingauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
