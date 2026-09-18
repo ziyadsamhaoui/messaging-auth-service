67e1e38805edbe201b1a6ee38b3e9be834f26cde
Ziyad SAMHAOUI <ziyad.samhaoui@usms.ac.ma>
2026-09-18 16:18:50 +0100

    fix: replaced placeholder env variables from compose.yaml and added missing dependencies to pom.xml

--- src/main/java/com/ziyadsamhaoui/messagingauthservice/MessagingAuthServiceApplication.java
+++ src/main/java/com/ziyadsamhaoui/messagingauthservice/MessagingAuthServiceApplication.java
@@ -1,13 +1,15 @@
 package com.ziyadsamhaoui.messagingauthservice;
 
+import com.ziyadsamhaoui.messagingauthservice.config.AuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthProperties.class)
 public class MessagingAuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MessagingAuthServiceApplication.class, args);
     }
-
 }
